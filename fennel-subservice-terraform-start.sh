a07776f2d890e70940c13059ac70c033eec0932f
Sean Batzel <romulus108@protonmail.com>
2023-06-15 21:30:28 -0400

    Startup script update.

--- fennel-subservice-terraform-start.sh
+++ fennel-subservice-terraform-start.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dit -p 6060:6060 --name subservice us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/subservice:latest
-docker exec -it subservice /app/npm run start
+docker run -dt -p 6060:6060 --name subservice us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/subservice:latest
+docker exec subservice /app/npm run start
