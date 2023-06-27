744628e10ee66e30fd453244bd8e346d6f87b856
Sean Batzel <romulus108@protonmail.com>
2023-06-26 20:56:55 -0400

    Trying to run in production.

--- fennel-subservice-terraform-start.sh
+++ fennel-subservice-terraform-start.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dt -p 6060:6060 --name subservice us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/subservice:latest
+docker run -dt -e NODE_ENV=production -p 6060:6060 --name subservice us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/subservice:latest
 docker exec subservice /app/npm run start
